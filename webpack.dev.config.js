1418822b9fba408d964ece306413cc2a52280205
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-05-25 16:09:29 +0545

    Setup for the Develovment .env

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world':'./src/hello-world.js',
+        'kiwi':'./src/kiwi.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: ''
     },
@@ -56,12 +59,18 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "Webpack Tutorial",
-            // filename: 'sub-folder/custom_filename.html',
-            // meta: {
-            //     viewport: 'width=dev-width, initial-scale=1',
-            // }
-            template: 'src/index.hbs'
+            filename: 'hello-world.html',
+            chunks: ['hello-world'],
+            title: "Hello World",
+            description: 'Hello World',
+            template: 'src/page-template.hbs'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'kiwi.html',
+            chunks: ['kiwi'],
+            title: "Kiwi",
+            description: 'Kiwi',
+            template: 'src/page-template.hbs'
         })
     ]
 }
